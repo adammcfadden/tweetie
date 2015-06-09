d72be07bc245bb74c2e6f4e934e9aaeb7e2f0172
james williams & adam mcfadden <student@epicodus.com>
2015-06-09 11:48:35 -0700

    added working form for adding tweets to the page

--- controllers/feed.js
+++ controllers/feed.js
@@ -1,4 +1,17 @@
 Tweet.FeedController = Ember.ArrayController.extend({
   sortProperties: ['date'],
-  sortAscending: false
+  sortAscending: false,
+  body: null,
+  id: null,
+  date: null,
+  actions: {
+    tweet: function() {
+      var body = this.get('body');
+      var id = Math.floor(Math.random() * 250);
+      var date = new Date();
+      var object = {id: id, body: body, date: date};
+      debugger;
+      tweets.addObject(object);
+    }
+  }
 });
--- routes/feed.js
+++ routes/feed.js
@@ -1,4 +1,8 @@
 Tweet.FeedRoute = Ember.Route.extend({
+  // controllerName: 'feedArray',
+  // renderTemplate: function() {
+  //   this.render('feed');
+  // },
   model: function() {
     return tweets;
   }
